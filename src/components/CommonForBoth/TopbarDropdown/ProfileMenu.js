03507328f503c7c88d556570ba9a058770951989
Merab <m.bauzhadze@gorgia.ge>
2024-10-01 01:19:33 +0400

    removed lock screen

--- src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -89,10 +89,10 @@ console.log(process.env);
             <i className="bx bx-wrench font-size-16 align-middle me-1" />
             {props.t("Settings")}
           </DropdownItem>
-          <DropdownItem tag="a" href="auth-lock-screen">
+          {/* <DropdownItem tag="a" href="auth-lock-screen">
             <i className="bx bx-lock-open font-size-16 align-middle me-1" />
             {props.t("Lock screen")}
-          </DropdownItem>
+          </DropdownItem> */}
           <div className="dropdown-divider" />
           <Link to="/logout" className="dropdown-item">
             <i className="bx bx-power-off font-size-16 align-middle me-1 text-danger" />
